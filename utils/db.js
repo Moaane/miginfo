d60b0c46dc4b019b204d664043a2913b3eb2e4f2
Moaane <gpro0150@gmail.com>
2024-06-29 04:11:18 +0700

    muhaha

--- utils/db.js
+++ utils/db.js
@@ -4,8 +4,10 @@ import { PrismaClient } from "@prisma/client";
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
+  console.log("Initializing Prisma Client in production mode");
   prisma = new PrismaClient();
 } else {
+  console.log("Initializing Prisma Client in development mode");
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
